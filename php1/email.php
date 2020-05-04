1bfdb24a1e6027061b43949e55c305461982fe3c
amit702 <64738702+amit702@users.noreply.github.com>
2020-05-04 17:14:13 +0530

    send
    nice

---
+++ php1/email.php
@@ -0,0 +1,27 @@
+<?php
+if(isset($_POST['subutton']))
+{
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $message=$_POST['message'];
+
+    
+    $txt="Name:".$name. "\r\n" ."Email:".$email. "\r\n" ."Message:".$message. "\r\n";
+
+    $to="dlatiwari707@gmail.com";
+
+    $subject="Contact:form for Amit Kumar";
+ 
+    $headers = "From: webmaster@example.com" . "\r\n" .
+"CC:dlatiwari707@gmail.com"; 
+    
+    
+if(mail($to,$subject,$txt,$headers))
+{
+    echo 'Email sent success.';
+}else{
+    echo 'Fail to send email.';
+}
+}
+
+?>
